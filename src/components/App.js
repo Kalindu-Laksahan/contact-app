cfb9c8dfcd11467ef334d351b3c2871ee3f97920
Kalindu Laksahan <68551659+Kalindu-Laksahan@users.noreply.github.com>
2023-07-19 18:54:33 +0530

    implemented contact list function

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,31 @@ import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+  const contacts = [
+    {id: '1',
+    name: 'Kalindu',
+    email: 'kalindu@gmail.com'
+    },
+    {id: '2',
+    name: 'Viraj',
+    email: 'Viraj@gmail.com'
+    },
+    {id: '3',
+    name: 'Vishwa',
+    email: 'Vishwa@gmail.com'
+    },
+    {id: '2',
+    name: 'Malith',
+    email: 'Malith@gmail.com'
+    }
+  ]
+  
   return (
-    <div>
+    <div clasName='ui container'>
       <Header/><br/><br/><br/>
       <AddContact/>
-      <ContactList/>
+      {/* props pass data from parent to child */}
+      <ContactList contacts={contacts}/>
 
     </div>
     
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,8 +2,23 @@ import React, { Component } from 'react'
 
 const ContactList = (props) => {
 
+    const renderContactList = props.contacts.map((contact) => {
+        return (
+            <div className='item'>
+                <div className='content'>
+                    <div className='header'>{contact.name}</div>
+                    <div>{contact.email}</div>
+                </div>
+                <i className='trash alternate outline icon'
+                    style={{ color: 'red', marginTop: '7px' }}
+                ></i>
+            </div>
+        );
+    });
+
     return (
-        <div className='ui celled list'>Contact List
+        <div className='ui celled list'>
+            {renderContactList}
         </div>
     );
 
