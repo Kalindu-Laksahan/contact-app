a881ca9b4c56b55c9285c9ab7bbfd1b00a2b593d
Kalindu Laksahan <68551659+Kalindu-Laksahan@users.noreply.github.com>
2023-07-21 18:34:45 +0530

    used hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,41 @@
-import React, { Component } from 'react'  // import React from 'react';
+import React, { Component,useState,useEffect } from 'react'  // import React from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
-  const contacts = [
-    {id: '1',
-    name: 'Kalindu',
-    email: 'kalindu@gmail.com'
-    },
-    {id: '2',
-    name: 'Viraj',
-    email: 'Viraj@gmail.com'
-    },
-    {id: '3',
-    name: 'Vishwa',
-    email: 'Vishwa@gmail.com'
-    },
-    {id: '2',
-    name: 'Malith',
-    email: 'Malith@gmail.com'
+   const [contacts, setContacts] = useState([]); // useState is a hook
+
+  // const contacts = [
+  //   {id: '1',
+  //   name: 'Kalindu',
+  //   email: 'kalindu@gmail.com'
+  //   },
+  //   {id: '2',
+  //   name: 'Viraj',
+  //   email: 'Viraj@gmail.com'
+  //   },
+  //   {id: '3',
+  //   name: 'Vishwa',
+  //   email: 'Vishwa@gmail.com'
+  //   },
+  //   {id: '2',
+  //   name: 'Malith',
+  //   email: 'Malith@gmail.com'
+  //   }
+  // ]
+    const addContactHandler = (contact) => {
+      console.log(contact);
+      setContacts([...contacts, contact]);
     }
-  ]
-  
+
+    useEffect(() => {}, [contacts]); // useEffect is a hook
+
   return (
     <div clasName='ui container'>
       <Header/><br/><br/><br/>
-      <AddContact/>
+      <AddContact addContactHandler={addContactHandler}/>
       {/* props pass data from parent to child */}
       <ContactList contacts={contacts}/>
 
